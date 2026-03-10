9a6b156601f8da3e7f1c9037a4250dac73c628e9
qq <mbp.local>
2026-03-11 00:08:46 +0800

    Modify the site and base parms for github page depolyment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
-  site: 'https://qinshujuan.github.io/johnq',
-  base: '/',
+  site: 'https://doublequeens.github.io',
+  base: '/johnq',
   trailingSlash: 'always',
   markdown: {
     remarkPlugins: [remarkGfm],
